fbea162eb98eb9ab027cabdd1730c0a8361f23a4
fr0der1c <frederic@Rock-Elysium.local>
2023-08-14 21:54:16 +0800

    upgrade selenium and try to solve timeout exception

--- driver.py
+++ driver.py
@@ -5,16 +5,12 @@ from selenium.webdriver.firefox.options import Options
 firefox_opt = Options()
 if os.getenv("IS_IN_CONTAINER"):
     firefox_opt.headless = True
+firefox_opt.set_preference("general.useragent.override",
+                           "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
+firefox_opt.set_preference("dom.webdriver.enabled", False)
+firefox_opt.set_preference('useAutomationExtension', False)
 
 
 def get_browser():
-    profile = webdriver.FirefoxProfile()
-    profile.set_preference("general.useragent.override", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
-    profile.set_preference("dom.webdriver.enabled", False)
-    profile.set_preference('useAutomationExtension', False)
-    profile.update_preferences()
-
-    browser = webdriver.Firefox(options=firefox_opt,
-                                firefox_profile=profile,
-                                desired_capabilities=webdriver.DesiredCapabilities.FIREFOX)
+    browser = webdriver.Firefox(options=firefox_opt)
     return browser
